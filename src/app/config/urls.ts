1a9a6aaeff0c5158b8447e875ac4b7cfff3389c8
JoseK21 <GitHub2446>
2019-11-22 12:09:10 -0600

    cambios de Sign Up

--- src/app/config/urls.ts
+++ src/app/config/urls.ts
@@ -1,6 +1,7 @@
 export class urls {
     // Api 
-    public static api = 'http://192.168.43.36:8800/EatOnTimeTECAPI/';
+    public static api = 'http://localhost:8800/EatOnTimeTECAPI/';
+    // public static api = 'http://192.168.43.36:8800/EatOnTimeTECAPI/';
     // DataBase
     /* public static mongodb = "mongodb+srv://JoseK21:<jni2D2whJSROwNwe>@tecplane-ny5al.azure.mongodb.net/test?retryWrites=true&w=majority" */
 
