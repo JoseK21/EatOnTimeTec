b4999bb8e89eda3f5fe649cf83699237420df60f
ShXck <mss97@hotmail.es>
2019-11-23 12:54:23 -0600

    xsdsd

--- src/app/shared/navbar/navbar-chef/navbar-chef.component.ts
+++ src/app/shared/navbar/navbar-chef/navbar-chef.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-navbar-chef',
@@ -10,6 +12,14 @@ export class NavbarChefComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  show(){
+    Swal.fire(
+      'Ordenes Confirmadas',
+      'No hay ordenes confirmadas por el momento',
+      'info'
+    )
   }
 
 }
